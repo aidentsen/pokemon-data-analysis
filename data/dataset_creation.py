5629341a909a76d16afbb2d609d1ab949e9bd193
atsen <aiden.tsen@gmail.com>
2024-09-13 10:14:34 +0100

    Trial code for creating full dataset

--- data/dataset_creation.py
+++ data/dataset_creation.py
@@ -1,5 +1,20 @@
 import pokebase as pb
-import numpy as np
 import pandas as pd
 
 from pokemondata import PokemonData
+
+rows_list = []
+
+final_gen_1_mon_in_dataset = 151
+
+for dex_num in range(1, final_gen_1_mon_in_dataset + 1):
+    print(dex_num)
+    original_variety = PokemonData(dex_num)
+    rows_list.append(original_variety.to_dict())
+    for variety in original_variety.varieties:
+        print(dex_num, variety)
+        additional_variety = PokemonData(dex_num, variety)
+        rows_list.append(additional_variety.to_dict())
+
+df = pd.DataFrame(rows_list)
+df.to_csv('gen_1_data.csv')
