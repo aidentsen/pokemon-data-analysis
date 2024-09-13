85a6d50887d1371640da6b6bad6361dc9ad740de
atsen <aiden.tsen@gmail.com>
2024-09-13 22:37:49 +0100

    Validation helper functions written and applied to each fetched element

--- data/pokemondata.py
+++ data/pokemondata.py
@@ -30,58 +30,133 @@ class PokemonData:
         'dratini', 'larvitar', 'bagon', 'beldum', 'gible', 'deino', 'goomy', 'jangmo-o', 'dreepy', 'frigibax'
     ]
 
-    def __init__(self, pokemon):
-        # Set the Pokémon and Species endpoints, since all other properties are derived from them
+    def __init__(self, pokemon, error_log_file):
         # The Pokédex number (413) and specific Pokémon name (e.g. 'wormadam-grass') both work
-        self.pokemon_data = pb.pokemon(pokemon)
-        self.species_data = pb.pokemon_species(self.pokemon_data.species.name)
+
+        # For logging errors during the process
+        self.error_log_file = error_log_file
+
+        try:
+            # Set the Pokémon, Species and name, since these are important properties
+            self.pokemon_data = pb.pokemon(pokemon)
+            self.species_data = pb.pokemon_species(self.pokemon_data.species.name)
+            self.name = self.pokemon_data.name
+        except AttributeError:
+            self.pokemon_data = "missing"
+            self.species_data = "missing"
+            self.name = pokemon
+            self.log_error("initialisation")
 
         # Basic useful Pokémon information
-        self.dex_num = self.species_data.id
-        self.name = self.pokemon_data.name
-        self.species = self.species_data.name
-        self.generation = self.get_generation()  # Custom method needed as Gen is given as a string
-        self.types = [pokemon_type.type.name for pokemon_type in self.pokemon_data.types]
-        self.abilities, self.hidden_ability = self.get_abilities()
-        self.varieties = self.get_varieties()
+        self.dex_num = self.safe_get_attr('id', self.species_data)
+        self.name = self.safe_get_attr('name', self.pokemon_data)
+        self.species = self.safe_get_attr('name', self.species_data)
+        self.generation = self.safe_get_method(self.get_generation)
+        self.types = self.safe_get_list('types', self.pokemon_data, lambda t: t.type.name)
+        self.abilities, self.hidden_ability = self.safe_get_method(self.get_abilities, is_list=True)
+        self.varieties = self.safe_get_method(self.get_varieties)
 
         # Supplemental Pokémon information
-        self.female_rate = self.species_data.gender_rate  # Note: in eighths, genderless Mons will give the value -1
-        self.has_gender_differences = self.species_data.has_gender_differences
-        self.capture_rate = self.species_data.capture_rate
-        self.growth_rate = self.species_data.growth_rate
-        self.base_happiness = self.species_data.base_happiness
+        self.female_rate = self.safe_get_attr('female_rate', self.species_data)  # In eighths, genderless is -1
+        self.has_gender_differences = self.safe_get_attr('has_gender_differences', self.species_data)
+        self.capture_rate = self.safe_get_attr('capture_rate', self.species_data)
+        self.growth_rate = self.safe_get_attr('growth_rate', self.species_data)
+        self.base_happiness = self.safe_get_attr('base_happiness', self.species_data)
 
         # Egg information
-        self.hatch_counter = self.species_data.hatch_counter
-        self.egg_groups = [egg_group.name for egg_group in self.species_data.egg_groups]
+        self.hatch_counter = self.safe_get_attr('hatch_counter', self.species_data)
+        self.egg_groups = self.safe_get_list('egg_groups', self.species_data, lambda e: e.name)
 
         # Stats, including Base Stat Total
-        self.hp, self.attack, self.defense, self.sp_attack, self.sp_defense, self.speed = self.get_stats()
-        self.bst = sum([self.hp, self.attack, self.defense, self.sp_attack, self.sp_defense, self.speed])
+        self.hp, self.attack, self.defense, self.sp_attack, self.sp_defense, self.speed = self.safe_get_stats()
+        try:
+            self.bst = sum([self.hp, self.attack, self.defense, self.sp_attack, self.sp_defense, self.speed])
+        except TypeError:
+            self.bst = 'missing'
 
-        # Evolution data
-        self.evolves_from = self.species_data.evolves_from_species
-        self.evolutionary_stage = self.get_evolutionary_stage()  # Note: -1 for single-stage, 0 for unevolved etc
+        # Evolution data - note evolutionary stage is -1 for single-stage, 0 for unevolved etc
+        self.evolves_from = self.safe_get_attr('evolves_from_species', self.species_data, is_obj=True)
+        self.evolutionary_stage = self.safe_get_method(self.get_evolutionary_stage)
 
-        # Category markers - methods written where the data is not naturally present in the API
-        self.is_starter = self.id_is_starter()
-        self.is_pseudo = self.id_is_pseudo()
-        self.is_legendary = self.species_data.is_legendary
-        self.is_mythical = self.species_data.is_mythical
-        self.is_baby = self.species_data.is_baby
-        self.is_ultra_beast = self.id_is_ultra_beast()
-        self.is_paradox = self.id_is_paradox()
+        # Category markers
+        self.is_starter = self.safe_get_method(self.id_is_starter)
+        self.is_pseudo = self.safe_get_method(self.id_is_pseudo)
+        self.is_legendary = self.safe_get_attr('is_legendary', self.species_data)
+        self.is_mythical = self.safe_get_attr('is_mythical', self.species_data)
+        self.is_baby = self.safe_get_attr('is_baby', self.species_data)
+        self.is_ultra_beast = self.safe_get_method(self.id_is_ultra_beast)
+        self.is_paradox = self.safe_get_method(self.id_is_paradox)
         self.is_mega = "-mega" in self.name
         self.is_totem = "-totem" in self.name
         self.is_gmax = "-gmax" in self.name
 
-        # Appearance and dimensions
-        self.color = self.species_data.color.name
-        self.shape = self.species_data.shape.name
-        self.height_m = self.pokemon_data.height / 10.0
-        self.weight_kg = self.pokemon_data.weight / 10.0
+        # Appearance
+        self.color = self.safe_get_attr('color', self.species_data, is_obj=True)
+        self.shape = self.safe_get_attr('shape', self.species_data, is_obj=True)
 
+        # Dimensions: need to be evaluated before dividing for units
+        self.height_m = self.safe_get_attr('height', self.pokemon_data)
+        if self.height_m != 'missing':
+            self.height_m /= 10.0
+        self.weight_kg = self.safe_get_attr('weight', self.pokemon_data)
+        if self.weight_kg != 'missing':
+            self.weight_kg /= 10.0
+
+    # Logging method for errors
+    def log_error(self, field):
+        if self.pokemon_data != "missing":  # Shouldn't run if there was an initialisation error
+            with open(self.error_log_file, 'a') as log_file:
+                log_file.write(f"Error with {self.name}: {field} could not be parsed\n")
+
+    # Start of helper functions to log errors
+    def safe_get_attr(self, attr, obj, is_obj=False):
+        try:
+            return getattr(obj, attr).name if is_obj else getattr(obj, attr)
+        except AttributeError:
+            self.log_error(attr)
+            return "missing attribute"
+
+    def safe_get_method(self, method, is_list=False):
+        try:
+            result = method()
+            return result
+        except AttributeError:
+            self.log_error(method.__name__)
+            return "missing attribute" if not is_list else ("missing attribute", "missing attribute")
+
+    def safe_get_list(self, attr, obj, transform):
+        try:
+            return [transform(item) for item in getattr(obj, attr)]
+        except AttributeError:
+            self.log_error(attr)
+            return "missing attribute"
+
+    def safe_get_stat(self, index):
+        try:
+            return self.pokemon_data.stats[index].base_stat
+        except AttributeError:
+            self.log_error(f"stat_{index}")
+            return "missing attribute"
+
+    def safe_get_stats(self):
+        try:
+            stat_dict = dict()
+            stat_object = self.pokemon_data.stats
+            for stat in stat_object:
+                stat_dict[stat.stat.name] = stat.base_stat
+            return (
+                stat_dict['hp'],
+                stat_dict['attack'],
+                stat_dict['defense'],
+                stat_dict['special-attack'],
+                stat_dict['special-defense'],
+                stat_dict['speed']
+            )
+        except AttributeError:
+            self.log_error("stats")
+            return ["missing attribute"] * 6
+
+    # Start of functions needed to get attributes
     def get_generation(self):  # Return the numerical Generation number
         generation_string = self.species_data.generation.name
         return PokemonData.generation_dict[generation_string]
@@ -110,21 +185,6 @@ class PokemonData:
 
     def get_primary_ability(self):  # Used internally to help identify some Pokémon categories
         return self.pokemon_data.abilities[0].ability.name
-
-    def get_stats(self):
-        # Return the stat spread of the given Pokémon
-        stat_dict = dict()
-        stat_object = self.pokemon_data.stats
-        for stat in stat_object:
-            stat_dict[stat.stat.name] = stat.base_stat
-        return (
-            stat_dict['hp'],
-            stat_dict['attack'],
-            stat_dict['defense'],
-            stat_dict['special-attack'],
-            stat_dict['special-defense'],
-            stat_dict['speed']
-        )
 
     def get_evolutionary_stage(self):
         # Get details on the Pokémon's evolutionary chain
@@ -172,17 +232,24 @@ class PokemonData:
         # Koraidon and Miraidon count as Paradox Mons, so their Abilities are also included here
         return self.get_primary_ability() in ("protosynthesis", "quark-drive", "orichalcum-pulse", "hadron-engine")
 
+    # Lots of repetition when handling attributes that can be lists, so turning it into a function
+    def process_list_attr(self, attr):
+        if isinstance(attr, list):
+            return " ".join(attr)
+        else:
+            return attr
+
+    # Returns a dictionary of all meaningful attributes. Any lists are converted into space-separated strings
     def to_dict(self):
-        # Returns a dictionary of all meaningful attributes. Any lists are converted into space-separated strings
         return {
             'dex_num': self.dex_num,
             'name': self.name,
             'species': self.species,
             'generation': self.generation,
-            'types': " ".join(self.types),
-            'abilities': " ".join(self.abilities),
+            'types': self.process_list_attr(self.types),
+            'abilities': self.process_list_attr(self.abilities),
             'hidden_ability': self.hidden_ability,
-            'varieties': " ".join(self.varieties),
+            'varieties': self.process_list_attr(self.varieties),
 
             'female_rate': self.female_rate,
             'has_gender_differences': self.has_gender_differences,
@@ -191,7 +258,7 @@ class PokemonData:
             'base_happiness': self.base_happiness,
 
             'hatch_counter': self.hatch_counter,
-            'egg_groups': " ".join(self.egg_groups),
+            'egg_groups': self.process_list_attr(self.egg_groups),
 
             'bst': self.bst,
             'hp': self.hp,
